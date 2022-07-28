3ffd260bf733c8d6e49cad39f4c4442b34f3577f
Shantanu Dey Anik <40882871+Shantanu2645@users.noreply.github.com>
2022-07-28 11:55:23 +0600

    Update homoglyph_telegram.py

--- homoglyph_telegram.py
+++ homoglyph_telegram.py
@@ -1,18 +1,23 @@
-from tokenize import Token
 import telebot
 from homoglyph_generator import homograph
 import os
+from dotenv import load_dotenv
 
-Token = os.getenv('TOKEN')
-print(Token)
 
-bot = telebot.TeleBot(Token)
+if __name__ == "__main__":
 
-@bot.message_handler()
-def msg_reply(m):
-    print()
-    homograph_text = homograph(m.text)
+    load_dotenv()
 
-    bot.reply_to(m, homograph_text)
-bot.polling()
+    token = os.getenv('TOKEN_KEY')
+    
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler()
+    def msg_reply(m):
+        print(m)
+        homograph_text = homograph(m.text)
+        bot.reply_to(m, homograph_text)
+
+
+    bot.polling()
 
